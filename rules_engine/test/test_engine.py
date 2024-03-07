da96f414c13827f3142789333d66d40c04075878
Ndifreke Ekott <ndy40.ekott@gmail.com>
2024-03-07 23:58:11 +0000

    test: Add test for combination clause

--- rules_engine/test/test_engine.py
+++ rules_engine/test/test_engine.py
@@ -9,3 +9,18 @@ class RuleEngineTests(TestCase):
         dict_rule = {"temperature": {"is above": 10}}
         input_data = {"temperature": 15}
         self.assertTrue(evaluate(dict_rule, input_data))
+
+    def test_rule_engine_evals_to_false(self):
+        dict_rule = {"temperature": {"is above": 10}}
+        input_data = {"temperature": 15}
+        self.assertTrue(evaluate(dict_rule, input_data))
+
+    def test_rule_engine_works_without_combination_clause_groupings(self):
+        dict_rule = {"temperature": {"is above": 10}, "age": {"is above": 10}}
+        input_data = {"temperature": 15, "age": 11}
+        self.assertTrue(evaluate(dict_rule, input_data))
+
+    def test_rule_engine_succeeds_with_combination_clause_groupings(self):
+        dict_rule = {'and': {"temperature": {"is above": 10}, "age": {"is above": 10}}}
+        input_data = {"temperature": 15, "age": 11}
+        self.assertTrue(evaluate(dict_rule, input_data))
